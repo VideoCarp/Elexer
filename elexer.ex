41d6a14ad15242a320b013dcc210e2e4925ad914
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2022-04-11 15:46:58 +1000

    Remove redundancies

--- elexer.ex
+++ elexer.ex
@@ -42,32 +42,3 @@ defmodule Lexer do
         uglex(0, [], String.length(input_str), input_str, singlecharh, multicharh, otherwise, "")
     end
 end
-
-# interfacing
-defmodule Handlers do
-    def alphanumeric(character) do
-        character >= "a" && character <= "z"
-    end
-    def singlechars(char) do
-        cond do
-            char == "(" ->
-                {true, :oparen}
-            char == ")" ->
-                {true, :cparen}
-            true ->
-                {false, :pass}
-        end
-    end
-
-    def multichars(char) do
-        cond do
-            alphanumeric(char) ->
-                {true, :identifier}
-            true ->
-                {false, :pass}
-        end
-    end
-end
-# len, input_str, singlecharh, multicharh, otherwise (optional)
-inp = IO.gets("Input your program: ")
-Lexer.lex(inp, &Handlers.singlechars/1, &Handlers.multichars/1) |> IO.inspect()
