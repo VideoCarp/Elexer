1965d22ebe72e74cd19257bbc8b5956cd2d3455d
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2022-04-11 15:21:20 +1000

    Update elexer.ex

--- elexer.ex
+++ elexer.ex
@@ -1,35 +1,39 @@
 defmodule Lexer do
     def nothing do
-       # does nothing
     end
     # lex int, list, int, string -> [{string, atom}]
-    def lex(current \\ 0, tokenstream \\ [], len, input_str, singlecharh, multicharh, otherwise, tmp) do
+    def uglex(current \\ 0, tokenstream \\ [], len, input_str, singlecharh, multicharh, otherwise, tmp) do
         char = String.at(input_str, current)
         {boolch, tag} = singlecharh.(char)
         {boolmh, tagm} = multicharh.(char)
         unless current >= len do
             cond do
 
+
                 boolmh ->
-                    lex(current + 1, tokenstream, len, input_str, singlecharh, multicharh, otherwise, tmp <> char)
+                    uglex(current + 1, tokenstream, len, input_str, singlecharh, multicharh, otherwise, tmp <> char)
 
 
                 boolch ->
                     if tmp == "" do
-                        lex(current + 1, [{char, tag} | tokenstream], len, input_str, singlecharh, multicharh, otherwise, "")
+                        uglex(current + 1, [{char, tag} | tokenstream], len, input_str, singlecharh, multicharh, otherwise, "")
                     else
-                        lex(current, [{tmp, tagm} | tokenstream], len, input_str, singlecharh, multicharh, otherwise, "")
+                        uglex(current, [{tmp, tagm} | tokenstream], len, input_str, singlecharh, multicharh, otherwise, "")
                     end
 
 
                 true -> 
                     otherwise.()
-                    lex(current + 1, tokenstream, len, input_str, singlecharh, multicharh, otherwise, "")
+                    uglex(current + 1, tokenstream, len, input_str, singlecharh, multicharh, otherwise, "")
             end
         else
             # we do this because tokens were prepended rather than appended
-            # it is actually faster than appending
+            # this is due to some complications within Elixir.
             Enum.reverse(tokenstream)
         end
+    end
+    # to save you from the ugliness of uglex -> 
+    def lex(len, input_str, singlecharh, multicharh, otherwise \\ &nothing/0) do
+        uglex(0, [], len, input_str, singlecharh, multicharh, otherwise, "")
     end
 end
