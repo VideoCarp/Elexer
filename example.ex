141bab4e3eac914929faee101b2e130154b11c1c
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2022-04-11 16:31:12 +1000

    Update example.ex

--- example.ex
+++ example.ex
@@ -1,16 +1,12 @@
 use elexer
-
-# interfacing
 defmodule Handlers do
     def alphanumeric(character) do
-        character >= "a" && character <= "z" # incomplete alphanumeric; more like lowercase
+        character >= "a" && character <= "z"
     end
     def singlechars(char) do
         cond do
             char == "(" ->
                 {true, :oparen}
-                # You should return {bool, tag}
-                # bool should be true when you want the token to be added. false otherwise.
             char == ")" ->
                 {true, :cparen}
             true ->
@@ -29,8 +25,4 @@ defmodule Handlers do
 end
 # len, input_str, singlecharh, multicharh, otherwise (optional)
 inp = IO.gets("Input your program: ")
-# Lexer.lex takes a string to lex, your handlers for single-character tokens, and your handlers for multiple-character tokens.
-# Read docs for further info (when they come out, if they aren't there)
-# Elexer simplifies everything for you, so that you can just return a boolean and a tag.
-# Whether it's for multiple or single character tokens.
 Lexer.lex(inp, &Handlers.singlechars/1, &Handlers.multichars/1) |> IO.inspect()
